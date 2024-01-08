256973079ceb9cd4499cd4afea6b358708801a79
Mayra Velazquez <116855716+MayVvelazquez@users.noreply.github.com>
2024-01-08 19:08:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 CORS(app)
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Ma*******@localhost/cruddos"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:EcommerceBooks@localhost/cruddos"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
